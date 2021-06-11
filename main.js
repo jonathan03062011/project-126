f91206042a1d6738e15f4742bc852aca4b2701b7
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-06-11 13:14:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
+song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload()
 {
-
+ song=loadSound("music.mp3");
 }
 
 function setup()
 {
-canvas=createCanvas(500,480);
+canvas=createCanvas(500,500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+
+posenet=ml5.poseNet(video,modelloaded);
+posenet.on('pose',gotposes);
+}
+
+function modelloaded()
+{
+    console.log("posenet is intialized");
+}
+
+function gotposes(results)
+{
+ if(results.length>0)
+ {
+     console.log(results);
+     
+ }
 }
 
 function draw()
 {
-image(video,0,0,500,480);
+ image(video,0,0,500,500);
+ 
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function stop()
+{
+    song.stop();
 }
