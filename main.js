4c541d392761b12ead1e5a8ce7688b7cee34216f
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-06-15 18:57:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+InNumberleftWristY=0;
+remove_decimal=0;
+volume=0;
 function preload()
 {
  song=loadSound("music.mp3");
@@ -29,20 +32,29 @@ function gotposes(results)
  if(results.length>0)
  {
      console.log(results);
-     
+     leftWristX=results[0].pose.leftWrist.x;
+     leftWristY=results[0].pose.leftWrist.y;     
  }
 }
 
 function draw()
 {
  image(video,0,0,500,500);
- 
+
+ fill('red');
+ stroke('red');
+
+ circle(leftWristX,leftWristY,20);
+ InNumberleftWristY=Number(leftWristY);
+ remove_decimal=floor(InNumberleftWristY);
+ volume=remove_decimal/500;
+ document.getElementById("volume").innerHTML="VOLUME="+volume;
 }
 
 function play()
 {
     song.play();
-    song.setVolume(1);
+    song.setVolume(volume);
     song.rate(1);
 }
 
